2f23ad36d7171c3de7c502174446eaae0bf0a901
samiha26 <samiha.tasnim143@gmail.com>
2022-01-23 14:10:56 +0800

    .

--- Assignment/resize_imgs.R
+++ Assignment/resize_imgs.R
@@ -10,7 +10,7 @@ for (i in imgs) {
   image_write(image = img, path = i)
 }
 
-
 img <- image_read("file/3.jpeg")
 img <- image_scale(img, "120x139")
 image_write(image = img, path = "file/3.jpeg")
+
